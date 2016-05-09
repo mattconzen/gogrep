8b85cf9102567c61ed69dccd0862993ac58681ee
Matt Conzen <mattconzen@gmail.com>
2016-05-09 00:56:23 -0500

    Removed default repo path to support other OS's (because I realized how dumb this was after I made my last commit from my linux machine...)

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 															 `String: File location for the CSV representation of
 															 	the results. (Default: output.csv)`)
 
-  repoLocationPtr := flag.String("repo", "C:\repo", `String: Repository location on disk.`)
+  repoLocationPtr := flag.String("repo", "", `String: Repository location on disk.`)
 
 	flag.Parse()
 
@@ -28,6 +28,7 @@ func main() {
   o, _ := os.Create(*outputFilePtr)
 	r := csv.NewReader(bufio.NewReader(f))
   w := csv.NewWriter(o)
+	repo := string(*repoLocationPtr)
 
 	for {
 		record, err := r.Read()
@@ -38,15 +39,17 @@ func main() {
 
 		//Print number of records read.
 		fmt.Println(len(record))
-		fmt.Println(string(*repoLocationPtr))
+		fmt.Println(repo)
 
-    dir, err := os.Open(string(*repoLocationPtr))
+		if repo != "" && len(repo) > 0 {
+	    dir, err := os.Open(repo)
 
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		} else {
-			dir.Chdir()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				dir.Chdir()
+			}
 		}
 
 		for value := range record {
